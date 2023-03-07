82fdb1553ed2833dba19af0fa1978e6149531934
Nadejda <vremea_denighi@mail.ru>
2023-03-07 14:05:10 +0000

    App structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+//Router
+import { Route, Routes } from 'react-router-dom';
+//MUI
+import { Box } from '@mui/system';
+//style
 import './App.css';
+//Components
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Box width='400px' sx={{ width: { xl: '1488px' }}} m='auto' >
+      <Navbar />
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <Route path='/exercise/:id' element={ <ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
---
+++ src/pages/ExerciseDetail.jsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom"
+
+export default function ExerciseDetail() {
+    const { id } = useParams()
+  return (
+    <div>
+    </div>
+  )
+}
