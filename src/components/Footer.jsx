d683daa70ddc0b7a6313ab266b3159bae3bd8464
Nadejda <vremea_denighi@mail.ru>
2023-03-18 19:16:38 +0000

    Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material"
+import Logo from '../assets/images/Logo-1.png'
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
+    <Box mt='70px' bgcolor='#fff3f4'>
+      <Stack
+        gap='40px'
+        alignItems='center'
+        px='40px'
+        pt='24px'
+      >
+        <img src={Logo} width='200px' height='40px' alt='logo' />
+        <Typography variant='h5' pb='40px' mt='20px'>
+          2023 &copy; Exercise GoldsGym
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
